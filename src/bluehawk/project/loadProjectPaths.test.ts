431da06d1f7f636639d94e40847f863722f1cadd
Chris Bush <chris.bush@mongodb.com>
2021-02-11 23:50:58 -0500

    Replace hand-rolled MockFs with memfs

--- src/bluehawk/project/loadProjectPaths.test.ts
+++ src/bluehawk/project/loadProjectPaths.test.ts
@@ -1,14 +1,15 @@
 import * as Path from "path";
-import { System } from "../io/System";
-import { MockFs } from "../io/MockFs";
+import { FileSystem, System } from "../io/System";
 import { loadProjectPaths } from "./loadProjectPaths";
+import { fs } from "memfs";
 
 describe("loadProjectPaths", () => {
-  System.fs = MockFs;
+  System.fs = fs.promises as FileSystem;
 
   it("loads project paths", async (done) => {
-    MockFs.reset();
-    System.fs.mkdir(Path.resolve("virtual/testProject/test"));
+    System.fs.mkdir(Path.resolve("virtual/testProject/test"), {
+      recursive: true,
+    });
     System.fs.writeFile(
       Path.resolve("virtual/testProject/test/foo.txt"),
       "hello, world!"
