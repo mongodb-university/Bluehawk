dc4a17452f9f8b9ac9c47ffc800be9000683d277
nathan-contino-mongo <nathan.contino@mongodb.com>
2021-02-18 17:27:31 -0700

    double-emph test

--- src/bluehawk/commands/emphasizeCommand.test.ts
+++ src/bluehawk/commands/emphasizeCommand.test.ts
@@ -7,6 +7,31 @@ describe("emphasize command", () => {
     commands: [EmphasizeCommand],
   });
 
+  it("works as a one line command", async (done) => {
+    const source = new Document({
+      text: `a
+// :emphasize:
+b
+c
+`,
+      language: "javascript",
+      path: "test.js",
+    });
+
+    const parseResult = bluehawk.parse(source);
+    const files = await bluehawk.process(parseResult);
+    expect(files["test.js"].source.text.toString()).toBe(
+      `a
+b
+c
+`
+    );
+    expect(
+      files["test.js"].source.attributes["emphasize"]["ranges"]
+    ).toStrictEqual([{ start: 2, end: 2 }]);
+    done();
+  });
+
   it("functions as a block", async (done) => {
     const singleInput = `const bar = "foo"
 
@@ -44,28 +69,45 @@ console.log(bar);
     done();
   });
 
-  it("works as a one line command", async (done) => {
+  it("handles double blocks correctly", async (done) => {
+    const singleInput = `const bar = "foo"
+
+// :emphasize-start:
+describe("some stuff", () => {
+  // :emphasize-end:
+  it("foos the bar", () => {
+    expect(true).toBeTruthy();
+  });
+  // :emphasize-start:
+});
+// :emphasize-end:
+console.log(bar);
+`;
+
     const source = new Document({
-      text: `a
-// :emphasize:
-b
-c
-`,
+      text: singleInput,
       language: "javascript",
       path: "test.js",
     });
 
     const parseResult = bluehawk.parse(source);
     const files = await bluehawk.process(parseResult);
-    expect(files["test.js"].source.text.toString()).toBe(
-      `a
-b
-c
-`
-    );
+    expect(files["test.js"].source.text.toString()).toBe(`const bar = "foo"
+
+describe("some stuff", () => {
+  it("foos the bar", () => {
+    expect(true).toBeTruthy();
+  });
+});
+console.log(bar);
+`);
+
     expect(
       files["test.js"].source.attributes["emphasize"]["ranges"]
-    ).toStrictEqual([{ start: 2, end: 2 }]);
+    ).toStrictEqual([
+      { start: 3, end: 3 },
+      { start: 7, end: 7 },
+    ]);
     done();
   });
 });
