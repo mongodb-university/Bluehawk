633f93512d66b71131fcdff7541f8141054b4781
Chris Bush <chris.bush@mongodb.com>
2021-03-02 20:56:54 -0500

    Update dependencies and clean up eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,18 @@
 module.exports = {
   env: {
     node: true,
-    es6: true,
-  },
-  parserOptions: {
-    sourceType: "module",
-    allowImportExportEverywhere: true,
-    ecmaVersion: 8,
   },
   root: true,
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
-  overrides: [
-    {
-      files: ["js-test/*.js"],
-      env: {
-        mocha: true,
-      },
-    },
-    {
-      files: ["*.ts"],
-      parser: "@typescript-eslint/parser",
-      plugins: ["@typescript-eslint", "eslint-plugin-tsdoc"],
-      extends: [
-        "eslint:recommended",
-        "prettier/@typescript-eslint",
-        "plugin:prettier/recommended",
-        "plugin:@typescript-eslint/recommended",
-      ],
-      rules: {
-        "tsdoc/syntax": "warn",
-      },
-    },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "eslint-plugin-tsdoc"],
+  extends: [
+    "eslint:recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
+  rules: {
+    "tsdoc/syntax": "warn",
+  },
   ignorePatterns: ["**/build/*"],
 };
