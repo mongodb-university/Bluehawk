ca65432e8983a345e778e2269de53c6c6b361f85
Chris Bush <chris.bush@10gen.com>
2021-07-27 11:03:34 -0400

    Copy permissions when copying files (#66)

--- src/cli/commands/copy.test.ts
+++ src/cli/commands/copy.test.ts
@@ -6,7 +6,7 @@ describe("copy", () => {
   beforeEach(getBluehawk.reset);
   beforeEach(System.useMemfs);
 
-  it("copies", async (done) => {
+  it("copies", async () => {
     const rootPath = "/path/to/project";
     const destinationPath = "/destination";
     await System.fs.mkdir(rootPath, {
@@ -26,7 +26,6 @@ describe("copy", () => {
     expect(sourceList).toStrictEqual(["test.txt"]);
     const destinationList = await System.fs.readdir(destinationPath);
     expect(destinationList).toStrictEqual(sourceList);
-    done();
   });
 
   it("copies binary files", async () => {
@@ -55,5 +54,37 @@ describe("copy", () => {
     const destinationList = await System.fs.readdir(destinationPath);
     expect(destinationList).toStrictEqual(sourceList);
     expect(didCallBinaryFileForPath).toBe(filePath);
+  });
+
+  it("copies permissions", async () => {
+    const rootPath = "/path/to/project";
+    const destinationPath = "/destination";
+    await System.fs.mkdir(rootPath, {
+      recursive: true,
+    });
+    await System.fs.mkdir(destinationPath, {
+      recursive: true,
+    });
+    const binPath = Path.join(rootPath, "test.bin");
+    await System.fs.writeFile(binPath, new Uint8Array([0, 1, 2, 3, 4, 5]));
+    await System.fs.chmod(binPath, 0o100755);
+    const textPath = Path.join(rootPath, "test.sh");
+    await System.fs.writeFile(textPath, "# this is a script", "utf8");
+    await System.fs.chmod(textPath, 0o100755);
+    const errors = await copy({
+      destination: destinationPath,
+      rootPath,
+    });
+
+    expect(errors).toStrictEqual([]);
+    const sourceList = await System.fs.readdir(rootPath);
+    expect(sourceList).toStrictEqual(["test.bin", "test.sh"]);
+    const destinationList = await System.fs.readdir(destinationPath);
+    expect(destinationList).toStrictEqual(sourceList);
+    const modes = [
+      await System.fs.stat(Path.join(destinationPath, "test.bin")),
+      await System.fs.stat(Path.join(destinationPath, "test.sh")),
+    ].map(({ mode }) => mode);
+    expect(modes).toStrictEqual([0o100755, 0o100755]);
   });
 });
