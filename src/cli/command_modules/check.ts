ed0320f69257677340a7202cd7a18a64d4d48640
Aleksander Binion <alek.binion@mongodb.com>
2021-09-22 16:36:14 -0400

    remove implementation from command module

--- src/cli/command_modules/check.ts
+++ src/cli/command_modules/check.ts
@@ -1,49 +1,7 @@
 import { CommandModule, Arguments } from "yargs";
-import { getBluehawk } from "../../bluehawk";
 import { withIgnoreOption, withJsonOption } from "../options";
 import { MainArgs } from "../cli";
-import { BluehawkError } from "../../bluehawk/BluehawkError";
-import { printJsonResult } from "../printJsonResult";
-import { logErrorsToConsole } from "../../bluehawk/OnErrorFunction";
-
-interface CheckArgs extends MainArgs {
-  paths: string[];
-  ignore?: string | string[];
-}
-
-export const check = async (args: Arguments<CheckArgs>): Promise<void> => {
-  const { ignore, json, paths, waitForListeners } = args;
-  const bluehawk = await getBluehawk();
-  const fileToErrorMap = new Map<string, BluehawkError[]>();
-
-  const addErrors = (filePath: string, errors: BluehawkError[]) => {
-    logErrorsToConsole(filePath, errors);
-    const existingErrors = fileToErrorMap.get(filePath) ?? [];
-    fileToErrorMap.set(filePath, [...existingErrors, ...errors]);
-  };
-
-  // Define the handler for generating snippet files.
-  bluehawk.subscribe(({ parseResult }) => {
-    const { errors, source } = parseResult;
-    if (errors.length !== 0) {
-      addErrors(source.path, errors);
-    }
-  });
-
-  // Run through all given source paths and process them.
-  await bluehawk.parseAndProcess(paths, {
-    ignore,
-    onErrors: addErrors,
-    waitForListeners: waitForListeners ?? false,
-  });
-
-  if (json) {
-    const errorsByPath = Object.fromEntries(Array.from(fileToErrorMap));
-    printJsonResult(args, errorsByPath);
-  }
-
-  process.exit(fileToErrorMap.size);
-};
+import { CheckArgs, check } from "../../bluehawk";
 
 const commandModule: CommandModule<MainArgs & { paths: string[] }, CheckArgs> =
   {
