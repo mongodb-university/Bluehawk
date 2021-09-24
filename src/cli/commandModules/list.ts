81ce58943b7433f509ffc565e16326a4559c0815
Aleksander Binion <alek.binion@mongodb.com>
2021-09-24 15:13:12 -0400

    remove unused builder

--- src/cli/commandModules/list.ts
+++ src/cli/commandModules/list.ts
@@ -1,12 +1,7 @@
 import * as yargs from "yargs";
-import { commandDir } from "../cli";
-import * as path from "path";
 
 const commandModule: yargs.CommandModule = {
   command: "list",
-  builder(yargs) {
-    return commandDir(yargs, path.join("commands", "list"));
-  },
   handler() {
     yargs.showHelp();
   },
